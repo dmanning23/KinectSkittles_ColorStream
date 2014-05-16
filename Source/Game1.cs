3c802b44b8c98a595caf507b6d2d7518fc3cfbf1
unknown <dmanning23@hotmail.com>
2014-05-15 22:55:28 -0500

    got kinect working

--- Source/Game1.cs
+++ Source/Game1.cs
@@ -1,6 +1,8 @@
+using Microsoft.Kinect;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
+using System.IO;
 
 namespace KinectSkittles
 {
@@ -9,6 +11,8 @@ namespace KinectSkittles
 	/// </summary>
 	public class Game1 : Game
 	{
+		#region Members
+
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 
@@ -16,6 +20,28 @@ namespace KinectSkittles
 
 		List<Skittle> Skittles { get; set; }
 
+		/// <summary>
+		/// Active Kinect sensor
+		/// </summary>
+		private KinectSensor sensor;
+
+		/// <summary>
+		/// Intermediate storage for the depth data received from the camera
+		/// </summary>
+		private DepthImagePixel[] depthPixels;
+
+		/// <summary>
+		/// Intermediate storage for the depth data converted to color
+		/// </summary>
+		private byte[] colorPixels;
+
+		Texture2D pixels;
+		Color[] pixelData_clear;
+
+		#endregion //Members
+
+		#region Methods
+		
 		public Game1()
 		{
 			graphics = new GraphicsDeviceManager(this);
@@ -53,7 +79,59 @@ namespace KinectSkittles
 			spriteBatch = new SpriteBatch(GraphicsDevice);
 
 			_circle = Content.Load<Texture2D>("circle");
+
+			pixels = new Texture2D(graphics.GraphicsDevice,
+				640,
+				480, false, SurfaceFormat.Color);
+			pixelData_clear = new Color[640 * 480];
+			for (int i = 0; i < pixelData_clear.Length; ++i)
+				pixelData_clear[i] = Color.Black;
+
+			// Look through all sensors and start the first connected one.
+			// This requires that a Kinect is connected at the time of app startup.
+			// To make your app robust against plug/unplug, 
+			// it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
+			foreach (var potentialSensor in KinectSensor.KinectSensors)
+			{
+				if (potentialSensor.Status == KinectStatus.Connected)
+				{
+					this.sensor = potentialSensor;
+					break;
+				}
+			}
+
+			if (null != this.sensor)
+			{
+				// Turn on the color stream to receive color frames
+				this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+				// Allocate space to put the depth pixels we'll receive
+				this.depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+
+				// Allocate space to put the color pixels we'll create
+				this.colorPixels = new byte[this.sensor.DepthStream.FramePixelDataLength * sizeof(int)];
+
+				// Add an event handler to be called whenever there is new color frame data
+				this.sensor.ColorFrameReady += this.SensorColorFrameReady;
+
+				// Start the sensor!
+				try
+				{
+					this.sensor.Start();
+				//	sensor.ColorStream.CameraSettings.BacklightCompensationMode = BacklightCompensationMode.CenterOnly;
+				}
+				catch (IOException)
+				{
+					this.sensor = null;
+				}
+			}
+
+			//if (null == this.sensor)
+			//{
+			//	this.statusBarText.Text = Properties.Resources.NoKinectReady;
+			//}
 		}
+
 
 		/// <summary>
 		/// UnloadContent will be called once per game and is the place to unload
@@ -61,7 +139,10 @@ namespace KinectSkittles
 		/// </summary>
 		protected override void UnloadContent()
 		{
-			// TODO: Unload any non ContentManager content here
+			if (null != this.sensor)
+			{
+				this.sensor.Stop();
+			}
 		}
 
 		/// <summary>
@@ -84,8 +165,6 @@ namespace KinectSkittles
 		{
 			GraphicsDevice.Clear(Color.Black);
 
-			Rectangle dest = new Rectangle();
-
 			spriteBatch.Begin();
 
 			for (int i = 0; i < Skittles.Count; i++)
@@ -95,7 +174,39 @@ namespace KinectSkittles
 
 			spriteBatch.End();
 
+			pixels.SetData<Color>(pixelData_clear);
+			spriteBatch.Begin();
+			spriteBatch.Draw(pixels, new Vector2(0, 0), null, Color.White);
+			spriteBatch.End();
+
 			base.Draw(gameTime);
 		}
+
+		/// <summary>
+		/// Event handler for Kinect sensor's ColorFrameReady event
+		/// </summary>
+		/// <param name="sender">object sending the event</param>
+		/// <param name="e">event arguments</param>
+		private void SensorColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+		{
+			using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+			{
+				if (colorFrame != null)
+				{
+					// Copy the pixel data from the image to a temporary array
+					colorFrame.CopyPixelDataTo(this.colorPixels);
+
+					 // Convert the depth to RGB
+					for (int colorIndex = 0; colorIndex < colorPixels.Length; colorIndex += 4)
+					{
+						pixelData_clear[colorIndex / 4].R = colorPixels[colorIndex+ 2];
+						pixelData_clear[colorIndex / 4].G = colorPixels[colorIndex + 1];
+						pixelData_clear[colorIndex / 4].B = colorPixels[colorIndex + 0];
+					}
+				}
+			}
+		}
+
+		#endregion //Methods
 	}
 }
